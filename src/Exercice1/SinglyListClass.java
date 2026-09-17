7edaa50c5c449e1638f14c66a27d27ee822e4ce1
Nicolas Moyenin <nicolas.moyenin@etu.univ-amu.fr>
2026-09-17 14:42:46 +0200

    Lien avec eclipse

---
+++ src/Exercice1/SinglyListClass.java
@@ -0,0 +1,49 @@
+package Exercice1;
+
+public class SinglyListClass {
+
+	private Node header;
+	private long size;
+		
+	
+	public SinglyListClass() {
+		header = null;
+		size = 0;
+	}
+	private static class Node {
+		
+		// Var
+		private Integer element;
+		private Node next;
+		
+		//Constructor
+		public Node (Integer s, Node n) {
+			this.element = s;
+			this.next = n;
+		}
+		
+		public Integer getElement() {
+			return element;
+		}
+		
+		public Node getNext() {
+			return next;
+		}
+		
+		public void setElement(Integer newElement) {
+			element = newElement;
+		}
+		
+		public void setNext(Node newNext) {
+			next = newNext;
+		}
+
+		@Override
+		public String toString() {
+			return "Node [element=" + element + ", next=" + next + "]";
+		}
+		
+	}
+	
+	
+}
